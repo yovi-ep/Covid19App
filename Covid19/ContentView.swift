51711465f02285b28ee95e54b7ca1c8f2ae401f7
yovi.putra <yovi121093@gmail.com>
2021-07-20 17:36:11 +0700

    Initial Commit

---
+++ Covid19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid19
+//
+//  Created by Yovi Eka Putra on 20/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Covid19/Covid19App.swift
@@ -0,0 +1,17 @@
+//
+//  Covid19App.swift
+//  Covid19
+//
+//  Created by Yovi Eka Putra on 20/07/21.
+//
+
+import SwiftUI
+
+@main
+struct Covid19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
